bd00e6d9d71bc1d6a10b9269bf7e6b5e7867e147
Rachel Wirtz <12850271+Rachel-Wirtz@users.noreply.github.com>
2022-12-19 22:13:30 +0100

    Video 7: uint24_t

--- Collection/rw.h
+++ Collection/rw.h
@@ -41,6 +41,22 @@ public:
     {
     }
 
+    constexpr auto operator[](const std::size_t index) const noexcept -> const char& {
+        return m_Data.at(index);
+    }
+
+    constexpr auto operator[](const std::size_t index) noexcept -> char& {
+        return m_Data.at(index);
+    }
+
+    constexpr auto at(const std::size_t index) const noexcept -> const char& {
+        return m_Data.at(index);
+    }
+
+    constexpr auto at(const std::size_t index) noexcept -> char& {
+        return m_Data.at(index);
+    }
+
     constexpr auto data(void) const noexcept -> const char* {
         return m_Data.data();
     }
@@ -108,7 +124,6 @@ public:
 private:
     array_type m_Data{};
 };
-
 
 template<typename T, std::endian Endian> requires std::is_scalar_v<T>
 class endian {
@@ -183,7 +198,7 @@ private:
         static_assert(
             std::endian::native == std::endian::little || std::endian::native == std::endian::big,
             "Currently only little or big endian platforms are supported by the endian API"
-            );
+        );
 
         if constexpr (FromEndian != Endian && sizeof(T) > 1) {
             std::reverse(m_Data.begin(), m_Data.end());
@@ -210,5 +225,98 @@ public:
 
 template<typename T>
 big_endian(const T&) -> big_endian<T>;
+
+struct uint24_t {
+    static constexpr uint32_t max    = (256 * 256 * 256) - 1;
+    static constexpr uint8_t  hibyte = std::endian::native == std::endian::little ? 2 : 0;
+    static constexpr uint8_t  lobyte = std::endian::native == std::endian::little ? 0 : 2;
+
+    constexpr uint24_t()                noexcept = default;
+    constexpr uint24_t(const uint24_t&) noexcept = default;
+    constexpr uint24_t(uint24_t&&)      noexcept = default;
+
+    constexpr uint24_t& operator=(const uint24_t&) noexcept = default;
+    constexpr uint24_t& operator=(uint24_t&&)      noexcept = default;
+
+    constexpr uint24_t(uint32_t u) noexcept
+        : m_Data{}
+    {
+        uint32_t wrapped_u32 = u % (max + 1);
+        m_Data[hibyte] = wrapped_u32 >> 16;
+        m_Data[1]      = (wrapped_u32 >> 8) & 0xFF;
+        m_Data[lobyte] = wrapped_u32 & 0xFF;
+    }
+
+    constexpr auto operator+=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this + other);
+    }
+
+    constexpr auto operator-=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this - other);
+    }
+
+    constexpr auto operator*=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this * other);
+    }
+
+    constexpr auto operator/=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this / other);
+    }
+
+    constexpr auto operator%=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this % other);
+    }
+
+    constexpr auto operator&=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this & other);
+    }
+
+    constexpr auto operator|=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this | other);
+    }
+
+    constexpr auto operator^=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this ^ other);
+    }
+
+    constexpr auto operator<<=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this << other);
+    }
+
+    constexpr auto operator>>=(const uint24_t& other) noexcept -> uint24_t& {
+        return (*this) = (*this >> other);
+    }
+
+    constexpr operator uint32_t () const noexcept {
+        return ((m_Data[hibyte] << 16) | m_Data[1] << 8 | m_Data[lobyte]);
+    }
+
+private:
+    std::array<uint8_t, 3> m_Data{};
+};
+
+using little_uint8_t  = little_endian<uint8_t>;
+using little_int8_t   = little_endian<int8_t>;
+using little_uint16_t = little_endian<uint16_t>;
+using little_int16_t  = little_endian<int16_t>;
+using little_uint32_t = little_endian<uint32_t>;
+using little_int32_t  = little_endian<int32_t>;
+using little_uint64_t = little_endian<uint64_t>;
+using little_int64_t  = little_endian<int64_t>;
+using little_float_t  = little_endian<float_t>;
+using little_double_t = little_endian<double_t>;
+using little_uint24_t = little_endian<uint24_t>;
+
+using big_uint8_t  = big_endian<uint8_t>;
+using big_int8_t   = big_endian<int8_t>;
+using big_uint16_t = big_endian<uint16_t>;
+using big_int16_t  = big_endian<int16_t>;
+using big_uint32_t = big_endian<uint32_t>;
+using big_int32_t  = big_endian<int32_t>;
+using big_uint64_t = big_endian<uint64_t>;
+using big_int64_t  = big_endian<int64_t>;
+using big_float_t  = big_endian<float_t>;
+using big_double_t = big_endian<double_t>;
+using big_uint24_t = big_endian<uint24_t>;
 
 RW_NAMESPACE_END
